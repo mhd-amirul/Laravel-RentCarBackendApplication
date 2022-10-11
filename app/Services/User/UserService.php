30d3f1faeba3d8917cd5537841aee5be755bf1ee
mhd-amirul <amirulm236@gmail.com>
2022-10-11 16:00:36 +0700

    Make User Service to implement pattern

---
+++ app/Services/User/UserService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Services\User;
+
+use App\Repository\User\IUserRepository;
+use App\Services\User\IUserService;
+use Illuminate\Support\Facades\Hash;
+
+class UserService implements IUserService
+{
+    private $userRepository;
+    public function __construct(IUserRepository $userRepository)
+    {
+        return $this->userRepository = $userRepository;
+    }
+
+    public function createUser($request)
+    {
+        $data = $request->all();
+        $data["password"] = Hash::make($data["password"]);
+
+        return $this->userRepository->create($data);
+    }
+}
