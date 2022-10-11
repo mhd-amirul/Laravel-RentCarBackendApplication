0f71436a537fe88f5460cac327afba803e32a93e
mhd-amirul <amirulm236@gmail.com>
2022-10-11 16:01:35 +0700

    register User repo and service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,14 @@
 
 namespace App\Providers;
 
+use App\Repository\Otp\IOtpRepository;
+use App\Repository\User\IUserRepository;
+use App\Repository\Otp\OtpRepository;
+use App\Repository\User\UserRepository;
+use App\Services\Otp\IOtpService;
+use App\Services\Otp\OtpService;
+use App\Services\User\IUserService;
+use App\Services\User\UserService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // User
+        $this->app->bind(IUserRepository::class, UserRepository::class);
+        $this->app->bind(IUserService::class, UserService::class);
+
     }
 
     /**
