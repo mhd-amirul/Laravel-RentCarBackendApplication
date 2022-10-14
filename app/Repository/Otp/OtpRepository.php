e86a9ef57b903fc55435d2466994fb607de341c3
mhd-amirul <amirulm236@gmail.com>
2022-10-14 23:25:46 +0700

    Make repo for model otpcode

---
+++ app/Repository/Otp/OtpRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Repository\Otp;
+
+use App\Models\otpCode;
+use App\Repository\Otp\IOtpRepository;
+
+class OtpRepository implements IOtpRepository
+{
+    private $otp;
+    public function __construct(otpCode $otp)
+    {
+        return $this->otp = $otp;
+    }
+
+    public function create($data)
+    {
+        return $this->otp->create($data);
+    }
+
+    public function where($data)
+    {
+        return $this->otp->where('email', $data)->first();
+    }
+
+    public function delete($data)
+    {
+        $data = $this->where($data);
+        return $data->delete();
+    }
+}
