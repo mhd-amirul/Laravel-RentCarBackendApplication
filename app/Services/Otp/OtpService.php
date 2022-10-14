0f524f7c5f21973500789a92415e93362bb01f00
mhd-amirul <amirulm236@gmail.com>
2022-10-14 23:26:48 +0700

    Make service for repo otp

---
+++ app/Services/Otp/OtpService.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Services\Otp;
+
+use App\Helpers\ResponseFormatter;
+use App\Repository\Otp\IOtpRepository;
+use App\Services\Otp\IOtpService;
+
+class OtpService implements IOtpService
+{
+    private $otpRepository;
+    public function __construct(IOtpRepository $otpRepository)
+    {
+        return $this->otpRepository = $otpRepository;
+    }
+
+    public function createOtp($request)
+    {
+        try {
+            $data = [
+                "email" => $request->email,
+                "otp" => rand(1000, 9999)
+            ];
+            return $this->otpRepository->create($data);
+        } catch (\Exception $th) {
+            throw ResponseFormatter::throwErr();
+        }
+    }
+
+    public function whereOtp($request)
+    {
+        try {
+            return $this->otpRepository->where($request);
+        } catch (\Exception $th) {
+            throw ResponseFormatter::throwErr();
+        }
+    }
+
+    public function verifyEmail($user, $otp, $request)
+    {
+        try {
+            if ($user) {
+                if ($otp && $user->email == $otp->email && $otp->otp == $request->otp) {
+                    return "success";
+                } else {
+                    return "otpfail";
+                }
+            } else {
+                return "userfail";
+            }
+        } catch (\Exception $th) {
+            throw ResponseFormatter::throwErr();
+        }
+    }
+
+    public function deleteOtp($request)
+    {
+        try {
+            return $this->otpRepository->delete($request);
+        } catch (\Exception $th) {
+            throw ResponseFormatter::throwErr();
+        }
+    }
+}
