c621978d1acd368346700f1d9f4c1145d68a1117
mhd-amirul <amirulm236@gmail.com>
2022-10-14 23:44:42 +0700

    Implement queue for sending mail

--- app/Notifications/emailNotification.php
+++ app/Notifications/emailNotification.php
@@ -9,7 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class emailNotification extends Notification
+class emailNotification extends Notification implements ShouldQueue
 {
     use Queueable;
     private $user, $otp;
