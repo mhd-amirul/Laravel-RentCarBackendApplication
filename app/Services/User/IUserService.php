8b3525d21e7e78d6e7db9e4828e6c743f19ea02c
mhd-amirul <amirulm236@gmail.com>
2022-10-15 14:00:59 +0700

    Repair user services

--- app/Services/User/IUserService.php
+++ app/Services/User/IUserService.php
@@ -5,4 +5,8 @@ namespace App\Services\User;
 interface IUserService {
     public function createUser($request);
     public function whereUser($request);
+    public function updateUser($user, $request);
+    public function checkPassword($user, $request);
+    public function sendMail($user, $otp);
+    public function saveUser($user);
 }
--- app/Services/User/UserService.php
+++ app/Services/User/UserService.php
@@ -2,9 +2,12 @@
 
 namespace App\Services\User;
 
+use App\Helpers\ResponseFormatter;
+use App\Notifications\emailNotification;
 use App\Repository\User\IUserRepository;
 use App\Services\User\IUserService;
 use Illuminate\Support\Facades\Hash;
+use DateTime;
 
 class UserService implements IUserService
 {
@@ -16,13 +19,60 @@ class UserService implements IUserService
 
     public function createUser($request)
     {
-        $data = $request->all();
-        $data["password"] = Hash::make($data["password"]);
-        return $this->userRepository->create($data);
+        try {
+            $data = $request->all();
+            $data["password"] = Hash::make($data["password"]);
+            return $this->userRepository->create($data);
+        } catch (\Exception $th) {
+            throw ResponseFormatter::throwErr();
+        }
+
     }
 
     public function whereUser($request)
     {
-        return $this->userRepository->where($request);
+        try {
+            return $this->userRepository->where($request);
+        } catch (\Exception $th) {
+            throw ResponseFormatter::throwErr();
+        }
+    }
+
+    public function updateUser($user, $request)
+    {
+        try {
+            return $this->userRepository->update($user, $request);
+        } catch (\Exception $th) {
+            throw ResponseFormatter::throwErr();
+        }
+    }
+
+    public function checkPassword($user, $request)
+    {
+        try {
+            if (!$user || !Hash::check($request->password, $user->password)) {
+                return "invalid";
+            }
+        } catch (\Exception $th) {
+            throw ResponseFormatter::throwErr();
+        }
+    }
+
+    public function sendMail($user, $otp)
+    {
+        try {
+            return $user->notify(new emailNotification($user, $otp));
+        } catch (\Throwable $th) {
+            throw ResponseFormatter::throwErr();
+        }
+    }
+
+    public function saveUser($user)
+    {
+        try {
+            return $this->userRepository->save($user);
+        } catch (\Throwable $th) {
+            throw ResponseFormatter::throwErr();
+        }
     }
 }
