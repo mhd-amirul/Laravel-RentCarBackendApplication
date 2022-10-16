81cd1d1326cb8916d585c8511e1a269c4735ab6d
mhd-amirul <amirulm236@gmail.com>
2022-10-16 23:20:33 +0700

    Repair resetPass method

--- app/Http/Controllers/profile/profileController.php
+++ app/Http/Controllers/profile/profileController.php
@@ -2,14 +2,12 @@
 
 namespace App\Http\Controllers\profile;
 
+use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\resetPassword;
 use App\Http\Requests\updateProfile;
-use App\Models\User;
 use App\Services\User\IUserService;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
 
 class profileController extends Controller
 {
@@ -40,29 +38,19 @@ class profileController extends Controller
 
     public function edit(updateProfile $request)
     {
-        $this->userService->updateUser(auth()->user()->email, $request);
-        $user = $this->userService->whereUser(auth()->user()->email);
-        return response()->json([
-            "status" => "OK",
-            "data" => $user
-        ], 200);
+        $user = $this->userService->updateUser(auth()->user()->email, $request);
+        return ResponseFormatter::success($user, "Profile has been updated");
     }
 
     public function resetPassword(resetPassword $request)
     {
         $user = $this->userService->whereUser(auth()->user()->email);
-        if (!Hash::check($request["oldpassword"], $user->password)) {
-            return response()->json([
-                "status" => "BAD_REQUEST",
-                "message" => "invalid password"
-            ], 400);
+        $check = $this->userService->checkPassword($user, $request);
+        if ($check == "invalid") {
+            return ResponseFormatter::error(null, "Password not match");
         }
-
-        $request["password"] = Hash::make($request["password"]);
-        $this->userService->updateUser($user->email, $request);
-        return response()->json([
-            "status" => "OK",
-            "message" => "password has been updated",
-        ], 200);
+        $request["password"] = Hash::make($request["new_password"]);
+        $this->userService->updateUser(auth()->user()->email, $request);
+        return ResponseFormatter::success($user, "password has been updated");
     }
 }
