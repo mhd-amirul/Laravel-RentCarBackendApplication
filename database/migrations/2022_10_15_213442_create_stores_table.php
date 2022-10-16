a88002ce575b717cf29e40960141358dd519ccb0
mhd-amirul <amirulm236@gmail.com>
2022-10-16 23:23:24 +0700

    make store migration

---
+++ database/migrations/2022_10_15_213442_create_stores_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->string("slug")->unique();
+            $table->string("name");
+            $table->string("owner");
+            $table->bigInteger("nik")->unique();
+            $table->string("user");
+            $table->string("address");
+            $table->string("coordinate");
+            $table->string("ktp")->nullable();
+            $table->string("siu")->nullable();
+            $table->string("img_owner")->nullable();
+            $table->string("img_store")->nullable();
+            $table->string("status")->default("disable");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores');
+    }
+};
