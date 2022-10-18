239c1f9417fb93c2606ceed2140bd7b53bfe406c
mhd-amirul <amirulm236@gmail.com>
2022-10-18 23:43:00 +0700

    make storeRequest file

---
+++ app/Http/Requests/storeRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class storeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            "name" => "required|min:5|max:20",
+            "owner" => "required|min:5|max:20",
+            "nik" => "required|digits:16",
+            "village" => "required",
+            "city" => "required",
+            "province" => "required",
+            "latitude" => "required",
+            "longitude" => "required",
+            "ktp" => "mimes:png,jpg|max:2048",
+            "siu" => "mimes:png,jpg|max:2048",
+            "img_owner" => "mimes:png,jpg|max:2048",
+            "img_store" => "mimes:png,jpg|max:2048",
+        ];
+    }
+}
