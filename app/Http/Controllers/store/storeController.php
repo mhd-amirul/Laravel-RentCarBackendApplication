3e2270fad84fcb47baaaa7d10912c713172d7985
mhd-amirul <amirulm236@gmail.com>
2022-10-18 23:45:38 +0700

    implement pattern to registerStore method

--- app/Http/Controllers/store/storeController.php
+++ app/Http/Controllers/store/storeController.php
@@ -2,56 +2,28 @@
 
 namespace App\Http\Controllers\store;
 
+use App\Helpers\handleFile;
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\storeRequest;
 use App\Models\store;
+use App\Services\Store\IStoreService;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 
 class storeController extends Controller
 {
-    public function registerStore(Request $request)
+    private $storeService;
+
+    public function __construct(IStoreService $storeService)
     {
-        $data = $request->all();
-        $rule = [
-            "name" => "required",
-            "owner" => "required",
-            "nik" => "required|digits:16",
-            "village" => "required",
-            "city" => "required",
-            "province" => "required",
-            "latitude" => "required",
-            "longitude" => "required",
-            "ktp" => "file",
-            "siu" => "file",
-            "img_owner" => "file",
-            "img_store" => "file",
+        return [
+            $this->storeService = $storeService
         ];
+    }
 
-        $val = Validator::make($data, $rule);
-        if ($val->fails()) {
-            return ResponseFormatter::error(null, $val->errors());
-        }
-
-        if ($request->has("ktp")) {
-            $filename = time().rand(1111,9999).".".$request->ktp->extension();
-            $destinationPath = "storage\image";
-            $data["ktp"] = $destinationPath."\\".$filename;
-            $request->ktp->move($destinationPath, $filename);
-        }
-
-        $data["slug"] = time() . rand(11111, 99999) . $data["user"];
-        $data["user"] = auth()->user()->email;
-        $data["coordinate"] = [
-            "latitude" => $data["latitude"],
-            "longitude" => $data["longitude"]
-        ];
-        $data["address"] = [
-            "village" => $data["village"],
-            "city" => $data["city"],
-            "province" => $data["province"]
-        ];
-        // $data = store::create($data);
-        return response()->json($data);
+    public function registerStore(storeRequest $request)
+    {
+        $store = $this->storeService->createStore($request);
+        return ResponseFormatter::success($store, "success");
     }
 }
