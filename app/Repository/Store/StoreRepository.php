6b70c7663762fb5d13f2bf3d6c1247d4a92a4e8f
mhd-amirul <amirulm236@gmail.com>
2022-10-18 23:44:26 +0700

    make create method

--- app/Repository/Store/StoreRepository.php
+++ app/Repository/Store/StoreRepository.php
@@ -1,10 +1,20 @@
 <?php
 
-namespace App\Services\Store;
+namespace App\Repository\Store;
 
+use App\Models\store;
 use App\Repository\Store\IStoreRepository;
 
 class StoreRepository implements IStoreRepository
 {
+    private $store;
+    public function __construct(store $store)
+    {
+        return $this->store = $store;
+    }
 
+    public function create($data)
+    {
+        return $this->store->create($data);
+    }
 }
