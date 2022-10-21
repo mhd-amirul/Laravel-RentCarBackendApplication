2f89bf37be6f29f400ca300667f9df8e2db189ec
mhd-amirul <amirulm236@gmail.com>
2022-10-21 19:43:07 +0700

    repair updateStore method

--- app/Http/Controllers/store/storeController.php
+++ app/Http/Controllers/store/storeController.php
@@ -9,7 +9,9 @@ use App\Http\Requests\storeRequest;
 use App\Models\store;
 use App\Services\Store\IStoreService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class storeController extends Controller
 {
@@ -25,7 +27,7 @@ class storeController extends Controller
     public function registerStore(storeRequest $request)
     {
         $store = $this->storeService->createStore($request);
-        return ResponseFormatter::success($store, "success");
+        return ResponseFormatter::success($store, "Store has been registered!");
     }
 
     public function agreementStore()
@@ -36,7 +38,7 @@ class storeController extends Controller
 
     public function updateStore(Request $request)
     {
-        $store = store::where("user", request()->query("user"))->first();
+        $store = store::where("user", auth()->user()->email)->first();
         $data = $request->all();
         $rule = [
             "name" => "nullable|min:5|max:20",
@@ -47,15 +49,48 @@ class storeController extends Controller
             "img_owner" => "nullable|mimes:png,jpg|max:2048",
             "img_store" => "nullable|mimes:png,jpg|max:2048",
         ];
+        $val = Validator::make($data, $rule);
+        if ($val->fails()) {
+            return ResponseFormatter::error(null, $val->errors());
+        }
         if ($request->has("ktp")) {
             if ($store->ktp) {
-                Storage::delete($store->ktp);
-                return response()->json($store->ktp);
+                File::delete($store->ktp);
             }
-            // $file = handleFile::image($data["ktp"], "ktp");
-            // $data["ktp"] = $file["fullpath"];
-            // $request->ktp->move($file["path"], $file["filename"]);
+            $filename = time().rand(1111,9999).".".$request["ktp"]->extension();
+            $path = "storage\image\ktp";
+            $data["ktp"] = $path."\\".$filename;
+            $request->ktp->move($path, $filename);
         }
+        if ($request->has("siu")) {
+            if ($store->siu) {
+                File::delete($store->siu);
+            }
+            $filename = time().rand(1111,9999).".".$request["siu"]->extension();
+            $path = "storage\image\siu";
+            $data["siu"] = $path."\\".$filename;
+            $request->siu->move($path, $filename);
+        }
+        if ($request->has("img_owner")) {
+            if ($store->img_owner) {
+                File::delete($store->img_owner);
+            }
+            $filename = time().rand(1111,9999).".".$request["img_owner"]->extension();
+            $path = "storage\image\img_owner";
+            $data["img_owner"] = $path."\\".$filename;
+            $request->img_owner->move($path, $filename);
+        }
+        if ($request->has("img_store")) {
+            if ($store->img_store) {
+                File::delete($store->img_store);
+            }
+            $filename = time().rand(1111,9999).".".$request["img_store"]->extension();
+            $path = "storage\image\img_store";
+            $data["img_store"] = $path."\\".$filename;
+            $request->img_store->move($path, $filename);
+        }
+
+        $store->update($data);
         return response()->json([$data, $store]);
     }
 }
