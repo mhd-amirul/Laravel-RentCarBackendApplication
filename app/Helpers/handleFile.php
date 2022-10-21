25752138de8def7cd9adb61d708cd834ca65702a
mhd-amirul <amirulm236@gmail.com>
2022-10-21 19:34:18 +0700

    delete method not use

--- app/Helpers/handleFile.php
+++ app/Helpers/handleFile.php
@@ -4,18 +4,5 @@ namespace App\Helpers;
 
 class handleFile
 {
-    protected static $feedback = [
-        "path" => "\\",
-        "filename" => "error",
-        "fullpath" => "error"
-    ];
 
-    public static function image($filename, $lastPath, $path = "storage\image\\")
-    {
-        $filename = time().rand(1111,9999).".".$filename->extension();
-        self::$feedback["path"] = $path.$lastPath;
-        self::$feedback["filename"] = $filename;
-        self::$feedback["fullpath"] = $path.$lastPath."\\".$filename;
-        return self::$feedback;
-    }
 }
