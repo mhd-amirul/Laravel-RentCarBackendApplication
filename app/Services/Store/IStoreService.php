8bb846443d2ba84fc9d2fa098da98256c30b199c
mhd-amirul <amirulm236@gmail.com>
2022-10-21 19:58:24 +0700

    make whereStore method on service

--- app/Services/Store/IStoreService.php
+++ app/Services/Store/IStoreService.php
@@ -4,6 +4,7 @@ namespace App\Services\Store;
 
 interface IStoreService {
     public function createStore($request);
+    public function whereStore($user);
     public function addFile($request, $lastPath);
     // public function updateFile();
     // public function renameFileandPath();
