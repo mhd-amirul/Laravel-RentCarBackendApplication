7a37685699fced85dfa4591af63cbd159ca70dd4
mhd-amirul <amirulm236@gmail.com>
2022-10-21 19:57:46 +0700

    make where method for store

--- app/Repository/Store/IStoreRepository.php
+++ app/Repository/Store/IStoreRepository.php
@@ -4,4 +4,6 @@ namespace App\Repository\Store;
 
 interface IStoreRepository {
     public function create($data);
+    public function where($user);
+    // public function update($user, $data);
 }
