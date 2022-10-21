a993f19be01a0d2769c932e5a1377c1e8ee16210
mhd-amirul <amirulm236@gmail.com>
2022-10-21 19:44:02 +0700

    make handle file method and repair registerStore method

--- app/Services/Store/IStoreService.php
+++ app/Services/Store/IStoreService.php
@@ -4,4 +4,7 @@ namespace App\Services\Store;
 
 interface IStoreService {
     public function createStore($request);
+    public function addFile($request, $lastPath);
+    // public function updateFile();
+    // public function renameFileandPath();
 }
