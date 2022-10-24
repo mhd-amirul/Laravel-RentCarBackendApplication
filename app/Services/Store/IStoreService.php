66ac72b425cd0a060d86f2a65e8279230cd6a38d
mhd-amirul <amirulm236@gmail.com>
2022-10-24 21:45:37 +0700

    make interface for updateStore method

--- app/Services/Store/IStoreService.php
+++ app/Services/Store/IStoreService.php
@@ -5,6 +5,7 @@ namespace App\Services\Store;
 interface IStoreService {
     public function createStore($request);
     public function whereStore($user);
+    public function updateStore($store, $data);
     public function addFile($request, $lastPath);
     // public function updateFile();
     // public function renameFileandPath();
