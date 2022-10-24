4ea750088cc83e01caaea771efe9d8ebf845dde4
mhd-amirul <amirulm236@gmail.com>
2022-10-24 21:54:39 +0700

    make function updateFile

--- app/Services/Store/IStoreService.php
+++ app/Services/Store/IStoreService.php
@@ -7,6 +7,6 @@ interface IStoreService {
     public function whereStore($user);
     public function updateStore($store, $data);
     public function addFile($request, $lastPath);
-    // public function updateFile();
+    public function updateFile($request, $lastPath, $store);
     // public function renameFileandPath();
 }
