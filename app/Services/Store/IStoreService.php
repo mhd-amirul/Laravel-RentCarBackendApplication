f93ebadd05d45eee694d1caa15337a1ed9de8f4b
mhd-amirul <amirulm236@gmail.com>
2022-10-25 21:20:36 +0700

    repair updateFile service

--- app/Services/Store/IStoreService.php
+++ app/Services/Store/IStoreService.php
@@ -6,7 +6,6 @@ interface IStoreService {
     public function createStore($request);
     public function whereStore($user);
     public function updateStore($store, $data);
-    public function addFile($request, $lastPath);
     public function updateFile($request, $lastPath, $store);
     // public function renameFileandPath();
 }
