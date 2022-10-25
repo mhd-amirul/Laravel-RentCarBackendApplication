d3bc3429d68267d5a30d4e88b0a63e21e7d32c17
mhd-amirul <amirulm236@gmail.com>
2022-10-25 21:19:26 +0700

    repair handlefile class

--- app/Helpers/handleFile.php
+++ app/Helpers/handleFile.php
@@ -4,5 +4,24 @@ namespace App\Helpers;
 
 class handleFile
 {
+    public static function renameFile($request, $lastPath)
+    {
+        return [
+            "filename" => time().rand(1111,9999).".".$request[$lastPath]->extension(),
+            "path" => "storage\image\\".$lastPath
+        ];
+    }
 
+    public static function addFile($request, $lastPath)
+    {
+        try {
+            if ($request->has($lastPath)) {
+                $name = self::renameFile($request, $lastPath);
+                $request[$lastPath]->move($name["path"], $name["filename"]);
+                return $name["path"]."\\".$name["filename"];
+            }
+        } catch (\Throwable $th) {
+            throw ResponseFormatter::throwErr("addFile Error!");
+        }
+    }
 }
