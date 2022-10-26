b2e2ee61b30176aa05682b3d6056be555378ffdb
mhd-amirul <amirulm236@gmail.com>
2022-10-26 13:15:15 +0700

    delete updateFile method

--- app/Services/Store/IStoreService.php
+++ app/Services/Store/IStoreService.php
@@ -6,6 +6,5 @@ interface IStoreService {
     public function createStore($request);
     public function whereStore($user);
     public function updateStore($store, $data);
-    public function updateFile($request, $lastPath, $store);
     // public function renameFileandPath();
 }
