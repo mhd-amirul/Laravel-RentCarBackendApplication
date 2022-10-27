5fbfac288a5f0aee75120f55378dba40f3b03ebb
mhd-amirul <amirulm236@gmail.com>
2022-10-28 01:01:18 +0700

    make userAgreement model

---
+++ app/Models/userAgreement.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Jenssegers\Mongodb\Eloquent\Model;
+
+class userAgreement extends Model
+{
+    use HasFactory;
+    protected $connection = 'mongodb';
+    protected $collection = 'user_agreements';
+    protected $fillable = [
+        "user",
+        "status"
+    ];
+}
