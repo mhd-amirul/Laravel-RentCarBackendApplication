0705a3154ac1157b3b03afbe922f22d110ec4e59
mhd-amirul <amirulm236@gmail.com>
2022-10-31 00:25:23 +0700

    repair store services

--- app/Services/Store/IStoreService.php
+++ app/Services/Store/IStoreService.php
@@ -6,5 +6,6 @@ interface IStoreService {
     public function createStore($request);
     public function whereStore($user);
     public function updateStore($store, $data);
+    public function saveStore($store);
     // public function renameFileandPath();
 }
