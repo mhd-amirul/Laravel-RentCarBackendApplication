966d992366e24237a4417d239c99d730d4014063
mhd-amirul <amirulm236@gmail.com>
2022-10-30 12:10:11 +0700

    make userAgreement repo

---
+++ app/Repository/userAgreement/UserAgreementRepository.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Repository\userAgreement;
+
+use App\Models\User;
+use App\Models\userAgreement;
+use App\Repository\User\IUserRepository;
+
+class UserAgreementRepository implements IUserAgreementRepository
+{
+    private $userAgreement;
+    public function __construct(userAgreement $userAgreement)
+    {
+        return $this->userAgreement = $userAgreement;
+    }
+
+    public function create($data)
+    {
+        return $this->userAgreement->create($data);
+    }
+
+    public function where($data)
+    {
+        return $this->userAgreement->where("user", "exists", ["email", $data])->first();
+    }
+}
