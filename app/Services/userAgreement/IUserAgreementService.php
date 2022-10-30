f04b88ad16bfa39be10c61880f40719e1126027a
mhd-amirul <amirulm236@gmail.com>
2022-10-30 12:10:56 +0700

    make userAgreement service and where create method

---
+++ app/Services/userAgreement/IUserAgreementService.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Services\userAgreement;
+
+interface IUserAgreementService {
+    public function createUserAgreement($request);
+    public function whereUserAgreement($request);
+}
