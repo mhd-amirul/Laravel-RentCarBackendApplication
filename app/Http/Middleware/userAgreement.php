103e4fe704242120ab9c68ff76e28d456eb4f99f
mhd-amirul <amirulm236@gmail.com>
2022-11-01 22:17:58 +0700

    make new middleware - userAgreement

---
+++ app/Http/Middleware/userAgreement.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Helpers\ResponseFormatter;
+use App\Models\userAgreement as ModelsUserAgreement;
+use Closure;
+use Illuminate\Http\Request;
+
+class userAgreement
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $userAgreement = ModelsUserAgreement::where("user", "exists", ["email" => auth()->user()->email])->first();
+        if ($userAgreement != null) {
+            return $next($request);
+        }
+        return ResponseFormatter::error(null, "please agree our condition and term first", 403);
+    }
+}
