d8a2c2605064ef87b19cfa8647e49702314ab0ed
mhd-amirul <amirulm236@gmail.com>
2022-11-02 23:38:46 +0700

    repair rule request

--- app/Http/Requests/verifyEmailRequest.php
+++ app/Http/Requests/verifyEmailRequest.php
@@ -24,7 +24,6 @@ class verifyEmailRequest extends FormRequest
     public function rules()
     {
         return [
-            "email" => "required",
             "otp" => "required",
         ];
     }
