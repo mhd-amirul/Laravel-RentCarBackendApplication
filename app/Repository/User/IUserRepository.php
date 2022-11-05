5973e0e9016894740d8fb18f600cba5c1835cc2f
mhd-amirul <amirulm236@gmail.com>
2022-11-05 17:21:24 +0700

    repair variable on update interface

--- app/Repository/User/IUserRepository.php
+++ app/Repository/User/IUserRepository.php
@@ -5,6 +5,6 @@ namespace App\Repository\User;
 interface IUserRepository {
     public function create($data);
     public function where($data);
-    public function update($email, $data);
+    public function update($user, $data);
     public function save($user);
 }
