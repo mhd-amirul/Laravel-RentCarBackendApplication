29025e747a2a7a4d0aa8ec4f7bed2449e4b6d661
mhd-amirul <amirulm236@gmail.com>
2022-11-08 07:42:50 +0700

    repair userAgremeent interface service

--- app/Services/userAgreement/IUserAgreementService.php
+++ app/Services/userAgreement/IUserAgreementService.php
@@ -3,7 +3,7 @@
 namespace App\Services\userAgreement;
 
 interface IUserAgreementService {
-    public function createUserAgreement($request);
-    public function whereUserAgreement($request);
-    public function deleteUserAgreement();
+    public function createUserAgreement($request, $store);
+    public function whereUserAgreementOne($store, $user);
+    public function deleteUserAgreement($store);
 }
