4e060e25d0abfc873c90e036bbb08c12c5289d15
mhd-amirul <amirulm236@gmail.com>
2022-11-13 17:29:33 +0700

    repair addCar function

--- app/Http/Controllers/store/carController.php
+++ app/Http/Controllers/store/carController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\store;
 use App\Helpers\handleFile;
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\addCarRequest;
 use App\Models\car;
 use App\Models\store;
 use App\Models\User;
@@ -13,30 +14,13 @@ use Illuminate\Support\Facades\Validator;
 
 class carController extends Controller
 {
-    public function addCar(Request $request)
+    public function addCar(addCarRequest $request)
     {
         $store = store::where("user_id", auth()->user()->_id)->first();
         $data = $request->all();
-        $rules = [
-            "plat" => "required|min:1",
-            "brand" => "required|max:20",
-            "model" => "required|max:20",
-            "varian" => "required|max:10",
-            "transmisi" => "required|max:10",
-            "tahun" => "required",
-            "warna" => "required|max:20",
-            "cc" => "required|max:10",
-            "seater" => "required|max:2",
-            "bbm" => "required|max:20",
-            "harga" => "required|max:20",
-            "image1" => "required|file"
-        ];
-
-        $val = Validator::make($data, $rules);
-        if ($val->fails()) {
-            return response()->json($val->errors());
-        }
         $data["image1"] = handleFile::addFile($request, "image1", "car");
+        $data["slug"] = time() . rand(11111, 99999) . $data["brand"];
+        $data["shop_id"] = $store->id;
         $data = car::create($data);
         return ResponseFormatter::success($data);
     }
